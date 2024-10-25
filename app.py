89349134675eeb91128239cd92f56f00b350d96e
Akash Ravichandran <akash.ravichandran@redingtongroup.com>
2024-10-25 13:01:41 +0530

    Updated app File

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 app = Flask(__name__)
 
-# Example bot response logic that uses the entire conversation history
 def generate_bot_response(conversation_history):
-
     if len(conversation_history) == 1:
         print("****First user query****")
         user_query_dict = conversation_history[0]
@@ -48,18 +46,12 @@ def index():
 
 @app.route('/bot_response', methods=['POST'])
 def bot_response():
-    data = request.get_json()  # Get the JSON data sent from the client
+    data = request.get_json()
     conversation_history = data.get('conversation_history', [])
-
     print(f"{conversation_history = }")
-
     if conversation_history:
-        # Generate response using the entire conversation history
         bot_response = generate_bot_response(conversation_history)
-
-        # Return the bot's response
         return jsonify({"status": "success", "bot_message": bot_response})
-    
     return jsonify({"status": "error", "message": "No conversation history provided."})
 
 if __name__ == '__main__':
